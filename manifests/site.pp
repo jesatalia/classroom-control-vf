45a5a539bcbbd80fa07961541cb8f9663122d7ae
jesatalia <jesatalia@yahoo.com>
2016-03-30 10:58:10 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,6 +33,16 @@ ini_setting { 'random ordering':
 # http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
 # node definitions.
 
+node default {
+# This is where you can declare classes for all nodes.
+# Example:
+# class { 'my_class': }
+if $::virtual != 'physical' {
+$vmname = capitalize($::virtual)
+notify { "This is a ${vmname} virtual machine.": }
+}
+}
+
 # The default node definition matches any node lacking a more specific node
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
